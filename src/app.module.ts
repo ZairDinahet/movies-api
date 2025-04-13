94a0bbee1eb112622f8a601c66d7751bb7a260ee
ZairDinahet <dinahetzair@gmail.com>
2025-04-13 00:34:40 -0300

    feat: Configure Users, Movies, Auth, and Prisma modules in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { MoviesModule } from './movies/movies.module';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, MoviesModule, AuthModule, PrismaModule],
 })
 export class AppModule {}
