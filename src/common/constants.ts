0dab69ef6e66b974c6049a41f2b1d04dac7a1d9b
ZairDinahet <dinahetzair@gmail.com>
2025-04-14 04:22:33 -0300

    feat: refactor roles constants to use enum for better type safety

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,4 +1,10 @@
-export const ADMIN = 'ADMIN';
-export const USER = 'USER';
+export enum RolesEnum {
+  ADMIN = 'ADMIN',
+  USER = 'USER',
+}
+
+export const ADMIN = RolesEnum.ADMIN;
+export const USER = RolesEnum.USER;
+
 export const ROLES_KEY = 'roles';
 export const IS_PUBLIC_KEY = 'isPublic';
