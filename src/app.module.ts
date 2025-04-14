a8830c7da590cee986e890945ef959e6e6ddd2c6
ZairDinahet <dinahetzair@gmail.com>
2025-04-14 03:18:21 -0300

    feat: Add GlobalJwtGuard as a provider in AppModule for JWT authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,16 @@ import { UsersModule } from './users/users.module';
 import { MoviesModule } from './movies/movies.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { APP_GUARD } from '@nestjs/core';
+import { GlobalJwtGuard } from './common/guards/global-auth.guard';
 
 @Module({
   imports: [UsersModule, MoviesModule, AuthModule, PrismaModule],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: GlobalJwtGuard,
+    },
+  ],
 })
 export class AppModule {}
