736326bebc94a97d4fd5624847add72c584883d6
ZairDinahet <dinahetzair@gmail.com>
2025-04-14 01:03:30 -0300

    feat: Add cookie parser middleware to main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ClassSerializerInterceptor } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
 
   const config = new DocumentBuilder()
     .setTitle('Cats example')
