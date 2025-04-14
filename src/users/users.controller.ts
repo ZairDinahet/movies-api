29f960b77ec5fd01ccf02d2b0c803ce670ee8c29
ZairDinahet <dinahetzair@gmail.com>
2025-04-14 05:25:43 -0300

    feat: add authentication and authorization to the user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,44 +7,93 @@ import {
   Delete,
   Patch,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiParam,
+  ApiQuery,
+  ApiBody,
+} from '@nestjs/swagger';
 import { GetUsersFilterDto } from './dto/get-users-filter.dto';
 import { UserDto } from './dto/user.dto';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { ADMIN } from 'src/common/constants';
 
 @ApiTags('Users')
 @Controller('users')
+@ApiBearerAuth()
+@UseGuards(RolesGuard)
+@Roles(ADMIN)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOperation({ summary: 'Create a new user' })
+  @ApiResponse({
+    status: 201,
+    description: 'User created successfully',
+    type: UserDto,
+  })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiBody({ type: CreateUserDto, description: 'New user data' })
   @Post()
   async create(@Body() createUserDto: CreateUserDto): Promise<UserDto> {
     return this.usersService.create(createUserDto);
   }
 
+  @ApiOperation({ summary: 'Get all users with optional filters' })
+  @ApiResponse({ status: 200, description: 'List of users', type: [UserDto] })
+  @ApiQuery({
+    name: 'email',
+    required: false,
+    description: 'Filter by email address',
+    type: String,
+  })
   @Get()
   async findAll(@Query() filterDto: GetUsersFilterDto): Promise<UserDto[]> {
     return this.usersService.findAll(filterDto);
   }
 
+  @ApiOperation({ summary: 'Get a user by ID' })
+  @ApiParam({ name: 'id', description: 'ID of the user to retrieve' })
+  @ApiResponse({ status: 200, description: 'User found', type: UserDto })
+  @ApiResponse({ status: 404, description: 'User not found' })
   @Get(':id')
   async findOne(@Param('id') id: string): Promise<UserDto | null> {
     return this.usersService.findOne(id);
   }
 
+  @ApiOperation({ summary: 'Update user details by ID' })
+  @ApiParam({ name: 'id', description: 'ID of the user to update' })
+  @ApiResponse({
+    status: 200,
+    description: 'User updated successfully',
+    type: UserDto,
+  })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
+  @ApiOperation({ summary: 'Soft delete a user by ID' })
+  @ApiParam({ name: 'id', description: 'ID of the user to delete' })
+  @ApiResponse({ status: 200, description: 'User soft-deleted successfully' })
   @Delete(':id/soft')
   async softDelete(@Param('id') id: string) {
     return this.usersService.softDelete(id);
   }
 
+  @ApiOperation({ summary: 'Hard delete a user by ID' })
+  @ApiParam({ name: 'id', description: 'ID of the user to delete' })
+  @ApiResponse({ status: 200, description: 'User hard-deleted successfully' })
   @Delete(':id/hard')
   async hardDelete(@Param('id') id: string) {
     return this.usersService.hardDelete(id);
